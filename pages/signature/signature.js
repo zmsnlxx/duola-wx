295b2caf4a1fc5bd1c49fbf3966068eef64a7084
liuxiang <zmsnlxx@163.com>
2021-02-27 23:44:25 +0800

    feat: 新增签收模块

--- pages/signature/signature.js
+++ pages/signature/signature.js
@@ -1,66 +1,39 @@
-// pages/signature/signature.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    labels: [
+      { label: '运输方量', key: 'capacity' },
+      { label: '车牌', key: 'carNo' },
+      { label: '司机', key: 'driverName' },
+      { label: '收货单位', key: 'projectName' },
+      { label: '物料名称', key: 'goodsName' },
+    ],
+    result: {
+      projectName: '我是收货单位名称',
+      goodsName: 'C40',
+      driverName: '金闪闪',
+      carNo: '甘E09870',
+      capacity: '16m³',
+    },
+    params: {
+      remark: '',
+      volume: ''
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  cancel() {
+    wx.navigateBack()
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  changeValue(e) {
+    this.setData({ 'params.volume': e.detail })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  change(e) {
+    this.setData({ 'params.remark': e.detail.value })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  goSign() {
+    console.log(this.data.params)
+    wx.navigateTo({ url: '/pages/sign/sign' })
   }
 })
