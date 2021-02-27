55c9f00af1f4ecaa0c8b8115cb9b410924f8417e
liuxiang <zmsnlxx@163.com>
2021-02-27 23:02:07 +0800

    feat: 新增运输单详情模块

--- components/infoCard/infoCard.js
+++ components/infoCard/infoCard.js
@@ -15,6 +15,10 @@ Component({
     jumpDetail() {
       const { transId, id } = this.data.info
       wx.navigateTo({ url: `/pages/transportOrder/transportOrder?id=${transId || id }` })
+    },
+    sign() {
+      const { transId, id } = this.data.info
+      wx.navigateTo({ url: `/pages/signature/signature?id=${transId || id }` })
     }
   },
 })
--- pages/transportOrder/transportOrder.js
+++ pages/transportOrder/transportOrder.js
@@ -1,12 +1,17 @@
 import { ajax } from '../../utils/http'
+
 Page({
   data: {
     result: {},
+    steps: [
+      { text: '步骤四', desc: '描述信息' },
+      { text: '步骤一', desc: '描述信息' },
+      { text: '步骤二', desc: '描述信息' },
+      { text: '步骤三', desc: '描述信息' },
+    ],
+    active: 0
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
     console.log(options.id)
     if (options.id) {
@@ -18,52 +23,11 @@ Page({
     }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  goSign() {
+    wx.navigateTo({ url: '/pages/signature/signature' })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  goFeedback() {
+    wx.navigateTo({ url: '/pages/addFeedback/addFeedback' })
   }
 })
