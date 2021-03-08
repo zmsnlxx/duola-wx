abd56c6ce1ebbf2b3f3d46f64140e395c7ab7b45
liuxiang <zmsnlxx@163.com>
2021-03-08 13:30:28 +0800

    feat: 更改地图图标

--- pages/otherCar/otherCar.js
+++ pages/otherCar/otherCar.js
@@ -15,14 +15,13 @@ Page({
     const markers = [
       {
         latitude: projectPosition.lat, longitude: projectPosition.lng,
-        iconPath: 'https://lh3.googleusercontent.com/proxy/aBTsMR4K-DLAFHzI7sYogGwLTUd8lcsYYxN5MObA4X4a81PFAAavGqIk-wtFmSDcl28jezWXRGdw6958tS7VgtWiyrCv2qnbDS3R6g',
-        width: 12,
-        height: 12,
+        iconPath: '../../public/image/map-one.png',
+        width: 16,
+        height: 16,
         label: {
           content: '',  //文本
           color: '#FFFFFF',  //文本颜色
           borderRadius: 100,  //边框圆角
-          borderColor: '#37FC8E',
           bgColor: 'rgba(193, 213, 212, 0.35)',  //背景色
           padding: 100,  //文本边缘留白
           anchorY: -100,
@@ -30,10 +29,11 @@ Page({
         },
       },
       {
-        latitude: mixingStationPosition.lat, longitude: mixingStationPosition.lng,
+        latitude: mixingStationPosition.lat,
+        longitude: mixingStationPosition.lng,
         iconPath: '../../public/image/mixingStationPosition-icon.png',
-        width: 12,
-        height: 12,
+        width: 16,
+        height: 16,
         label: {
           content: mixingStationName,  //文本
           color: '#000000',  //文本颜色
@@ -49,11 +49,11 @@ Page({
           latitude: item.latitude,
           longitude: item.longitude,
           id: item.carId,
-          iconPath: 'https://lh3.googleusercontent.com/proxy/aBTsMR4K-DLAFHzI7sYogGwLTUd8lcsYYxN5MObA4X4a81PFAAavGqIk-wtFmSDcl28jezWXRGdw6958tS7VgtWiyrCv2qnbDS3R6g',
-          width: 12,
-          height: 12,
+          iconPath: '../../public/image/map-two.png',
+          width: 16,
+          height: 16,
           label: {
-            anchorY: -40,
+            anchorY: -46,
             content: item.carNo,  //文本
             color: '#FFFFFF',  //文本颜色
             borderRadius: 4,  //边框圆角
@@ -65,17 +65,6 @@ Page({
       })
       this.setData({ list, markers: this.data.markers.concat(markers) })
     })
-
-    // wx.getLocation({
-    //   type: 'gcj02',
-    //   isHighAccuracy: true,
-    //   success: res => {
-    //     const { latitude, longitude } = res
-    //     const { lng, lat } = this.convert2TecentMap(longitude, latitude)
-    //     that.setData({ latitude: lat, longitude: lng })
-    //   },
-    //   fail: () => Toast({ type: 'fail', context: this, message: '获取位置信息失败' }),
-    // })
   },
   convert2TecentMap(lng, lat) {
     if (lng === '' && lat === '') {
